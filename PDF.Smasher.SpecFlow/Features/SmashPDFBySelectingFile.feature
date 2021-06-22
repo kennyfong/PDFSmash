bfab778fd3bd2aa82bbc93048763098a2fcb50ba
kennyfong <kennyfon@gmail.com>
2021-06-22 22:02:54 +0100

    Setup SpecFlow project

---
+++ PDF.Smasher.SpecFlow/Features/SmashPDFBySelectingFile.feature
@@ -0,0 +1,11 @@
+﻿Feature: SmashPDFBySelectingFile
+	Flatten a PDF document so that users can digitally sign it
+
+@mytag
+Scenario: Flatten a PDF document
+	Given I launch the application
+	And I click on the Upload PDF box
+	And I select a PDF that is certified
+	And I click on the smash button
+	And I confirm the smash
+	Then the application will download the same PDF but without the certification
---
+++ PDF.Smasher.SpecFlow/Steps/SmashPDFBySelectingFileSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace PDF.Smasher.SpecFlow.Steps
+{
+    [Binding]
+    public class SmashPDFBySelectingFileSteps
+    {
+        [Given(@"I launch the application")]
+        public void GivenILaunchTheApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I click on the Upload PDF box")]
+        public void GivenIClickOnTheUploadPDFBox()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I select a PDF that is certified")]
+        public void GivenISelectAPDFThatIsCertified()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I click on the smash button")]
+        public void GivenIClickOnTheSmashButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I confirm the smash")]
+        public void GivenIConfirmTheSmash()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the application will download the same PDF but without the certification")]
+        public void ThenTheApplicationWillDownloadTheSamePDFButWithoutTheCertification()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
